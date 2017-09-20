e87b1ccfd27f4befbb1f55a88ffd346b63b22f71
Zack <zackstrikesback@gmail.com>
2017-09-19 16:49:31 -1000

    upload

---
+++ Settings/app/src/main/java/com/example/user/settings/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.example.user.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // Display SettingsFragment as the main content
+        getFragmentManager().beginTransaction()
+                .replace(R.id.uxFragmentContainer, new SettingsFragment())
+                .commit();
+    }
+}
